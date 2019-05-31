5398633d14a96befd8d64a289669a26bfc3bcbd5
Kayla <6lackmacaroon@gmail.com>
2019-05-31 09:49:46 -0600

    created router paths and endpooints

--- api/server.js
+++ api/server.js
@@ -1,14 +1,23 @@
 const express = require('express');
-const helpet = require('helmet');
+// const helmet = require('helmet');
+
+const actionRouter = require('../data/routers/actions/actionRouter.js');
+const projectRouter = require('../data/routers/projects/projectRouter.js');
 
 const server = express();
+
+server.use(express.json());
+// server.use(helmet());
 
 server.get('/', (req, res) => {
       res.send(`<h1>This is it, don't get scared now.</h1>`).json({ message: "You're doing it, Peter!"})
       .catch(err => {
-            console.log(`\nERROR`, error);
+            console.log(`\nERROR`, err);
             res.status(500).json({ error: 'cannot. even.' });
       })
 });
+
+server.use('/api/actions', actionRouter);
+server.use('/api/projects', projectRouter);
 
 module.exports = server;
