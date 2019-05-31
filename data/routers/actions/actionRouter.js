91628a8014b3edd87cdab7d95d2e6df8f10bd46b
Kayla <6lackmacaroon@gmail.com>
2019-05-31 11:26:27 -0600

    stuck on post

--- data/routers/actions/actionRouter.js
+++ data/routers/actions/actionRouter.js
@@ -1,4 +1,5 @@
 const Actions = require('../../helpers/actionModel');
+const Projects = require('../../helpers/projectModel');
 
 const router = require('express').Router();
 
@@ -13,18 +14,37 @@ router.get('/', (req, res) => {
       })
 });
 
-router.post('/', (req, res) => {  
-      console.log(req.body)    
-      Actions.insert(req.body)
+//post route doesnt work!
+router.post('/:id/project', (req, res) => {
+      const newAction = req.body;
+      console.log("action", req.body);
+      const id = req.params.id;
+      newAction.project_id = id;
+      
+      console.log('id', id)
+      Projects.insert(newAction)
       .then(action => {
             res.status(201).json(action);
             // 201 CREATED
       })
       .catch(err => {
-            res.status(500).json({ error: 'could not post new action to database'});
+            console.log(err);
+            res.status(500).json({ message: 'Error creating a new project for that action' });
       })
 });
 
+//middlewares
+// async function validateUserId(req, res, next) {
+//       const user = await Actions.getById(req.params.id)
+//       if (user) {
+//             // console.log('user', user)
+//             req.user = user
+//             next();
+//       } else {
+//             // console.log('user', user)
+//             res.status(400).json({ message: "invalid user id" })
+//       }
 
+// };
 
 module.exports = router;
