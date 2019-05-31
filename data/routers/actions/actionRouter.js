fddd3065444b7a05fc0e80e8f763a2f78c845581
Kayla <6lackmacaroon@gmail.com>
2019-05-31 13:10:16 -0600

    put and delete for action router built

--- data/routers/actions/actionRouter.js
+++ data/routers/actions/actionRouter.js
@@ -33,6 +33,34 @@ router.post('/:id/project', (req, res) => {
       })
 });
 
+router.put('/:id', async (req, res) => {
+      try {
+            const user = await Actions.update(req.params.id, req.body);
+            if (user) {
+                  res.status(200).json(user);
+            } else {
+                  res.status(404).json({ message: 'That action could not be found' });
+            }
+      } catch (err) {
+            console.log(err);
+            res.status(500).json({ message: 'Error updating the action' });
+      }
+});
+
+router.delete('/:id', async (req, res) => {
+      try {
+            const count = await Actions.remove(req.params.id);
+            if (count > 0) {
+                  res.status(200).json({ message: 'This action no longer exists.'});
+            } else {
+                  res.status(404).json({ message: 'That action could not be found'});
+            }
+      } catch (err) {
+            console.log(err);
+            res.status(500).json({ message: 'Error removing action' });
+      }
+});
+
 //middlewares
 // async function validateUserId(req, res, next) {
 //       const user = await Actions.getById(req.params.id)
