bcdd4836b1fa47152b20a34ca0b35dd97c5cc6cc
Kayla <6lackmacaroon@gmail.com>
2019-05-31 09:43:43 -0600

    build req handler for '/'

--- api/server.js
+++ api/server.js
@@ -2,3 +2,13 @@ const express = require('express');
 const helpet = require('helmet');
 
 const server = express();
+
+server.get('/', (req, res) => {
+      res.send(`<h1>This is it, don't get scared now.</h1>`).json({ message: "You're doing it, Peter!"})
+      .catch(err => {
+            console.log(`\nERROR`, error);
+            res.status(500).json({ error: 'cannot. even.' });
+      })
+});
+
+module.exports = server;
